0cf9e384857fefe1abcce11ca05a98b79cc8ee1c
CHO <bsuysheep927@gmail.com>
2022-01-20 12:43:54 +0800

    修改 Banner 左右低中間高、手機版一次輪播一個

--- js/bigbanner.js
+++ js/bigbanner.js
@@ -16,6 +16,20 @@ $(function() {
 		  nextEl: ".swiper-button-next",
 		  prevEl: ".swiper-button-prev",
 		},
+		breakpoints: {
+			// when window width is >= 320px
+			320: {
+			  slidesPerView: 1,
+			  spaceBetween: 0,
+			  slidesPerGroup: 1,
+			},
+
+			768: {
+				slidesPerView: 3,
+				spaceBetween: 0,
+				slidesPerGroup: 3,
+			}
+		  }
 	});
 
   });
