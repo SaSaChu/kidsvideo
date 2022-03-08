63f99a4e6c962434ac1396169593f20a19e1d0b8
SaSaChu <teresa7397@gmail.com>
2022-03-08 14:15:39 +0800

    更新項目

--- js/public.js
+++ js/public.js
@@ -13,21 +13,21 @@ $(function() {
     $('.m_menu').removeClass('menu_open');
   });
 
-  if(window.Swiper) {
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      slidesPerGroup: 4,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
-  }
+//   if(window.Swiper) {
+//     var swiper = new Swiper(".mySwiper", {
+//       slidesPerView: 4,
+//       spaceBetween: 30,
+//       slidesPerGroup: 4,
+//       loop: true,
+//       loopFillGroupWithBlank: true,
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//     });
+//   }
 });
