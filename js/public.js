cb87f8a3f2a33a0d1e0495f9e8ab63842b8a0fe3
SaSaChu <teresa7397@gmail.com>
2022-03-09 12:50:39 +0800

    修改- 客- v1

--- js/public.js
+++ js/public.js
@@ -13,6 +13,20 @@ $(function() {
     $('.m_menu').removeClass('menu_open');
   });
 
+  // $('.menu').click(function() {
+  //   $('.m01-h, .m02-h, .m03-h, .m04-h, .m05-h, .m06-h, .m07-h, .m09-h, .m10-h, .m11-h,').removeClass('dott');
+  // });
+
+  $('.menu').click(function() {
+    $(this).prev('.col').each (function () {
+      var $that = $(this);
+      $(this).removeClass ('dott');
+    });
+  });
+
+
+
+
 //   if(window.Swiper) {
 //     var swiper = new Swiper(".mySwiper", {
 //       slidesPerView: 4,
